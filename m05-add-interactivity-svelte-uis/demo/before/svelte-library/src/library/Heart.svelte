cfe07b174c1051c654fc88dc4e464c29c328a8a0
jaketrent <trent.jake@gmail.com>
2020-01-23 06:30:29 -0700

    m5 demo before

---
+++ m05-add-interactivity-svelte-uis/demo/before/svelte-library/src/library/Heart.svelte
@@ -0,0 +1,18 @@
+<style>
+  div {
+    align-items: center;
+    background: #ea7025;
+    border-radius: 100%;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3), 4px 8px 14px rgba(0, 0, 0, 0.2);
+    color: var(--colorFgInverse);
+    display: flex;
+    font-size: var(--typeSizeSmall);
+    font-weight: var(--typeWeightBold);
+    height: 2.5rem;
+    justify-content: center;
+    transform: rotate(-90deg);
+    width: 2.5rem;
+  }
+</style>
+
+<div>&lt;3</div>
