8fc06c3152f5b32216bd4b1314e39d1aba4fccba
jaketrent <trent.jake@gmail.com>
2020-01-25 20:14:18 -0700

    m5 switch to detail with page state

--- m05-add-interactivity-svelte-uis/demo/after/svelte-library/src/App.svelte
+++ m05-add-interactivity-svelte-uis/demo/after/svelte-library/src/App.svelte
@@ -1,8 +1,15 @@
 <script>
+  import Detail from "./detail/Detail.svelte";
   import Library from "./library/Library.svelte";
+
+  let page = "library";
+  let pageArgs = {};
 
   function handleBookSelect(event) {
     console.log("app handle", event.detail.id);
+
+    page = "detail";
+    pageArgs = event.detail;
   }
 </script>
 
@@ -13,5 +20,9 @@
 </style>
 
 <main>
-  <Library on:book-select={handleBookSelect} />
+  {#if page === 'library'}
+    <Library on:book-select={handleBookSelect} />
+  {:else if page === 'detail'}
+    <Detail {...pageArgs} />
+  {/if}
 </main>
