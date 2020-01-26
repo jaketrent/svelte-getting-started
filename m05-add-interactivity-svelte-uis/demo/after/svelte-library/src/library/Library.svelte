d5026beff195852b7a86dcdbb3f316732e06310e
jaketrent <trent.jake@gmail.com>
2020-01-25 19:53:15 -0700

    m5 after start

---
+++ m05-add-interactivity-svelte-uis/demo/after/svelte-library/src/library/Library.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { onMount } from "svelte";
+
+  import BookGrid from "./BookGrid.svelte";
+  import Button from "../common/Button.svelte";
+  import { httpGet } from "../common/api.js";
+
+  let books = [];
+  onMount(async function() {
+    const { data } = await httpGet("/?_sort=id&_order=desc");
+    books = data;
+  });
+</script>
+
+<style>
+  header {
+    margin: var(--spacingMedium) 0 var(--spacingLarge) 0;
+    text-transform: uppercase;
+  }
+  .preamble {
+    display: block;
+  }
+  h1 {
+    font-size: var(--typeSizeXXLarge);
+    font-weight: var(--typeWeightBold);
+    line-height: var(--typeLineHeightTight);
+  }
+
+  .greeting {
+    font-size: var(--typeSizeSmall);
+  }
+</style>
+
+<header>
+  <span class="preamble">Welcome to the</span>
+  <h1>Library</h1>
+</header>
+
+<p class="greeting">
+  This is a library for the people. Welcome. Read the books here. Be inspired.
+  Go home, and share them with your family.
+</p>
+
+<Button>+ Add Book</Button>
+
+<BookGrid {books} />
