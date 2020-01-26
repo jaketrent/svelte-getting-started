9a26a80751e2727137141b68d73ba36f59eb98ca
jaketrent <trent.jake@gmail.com>
2020-01-25 20:17:32 -0700

    m5 request detail book with id

--- m05-add-interactivity-svelte-uis/demo/after/svelte-library/src/detail/Detail.svelte
+++ m05-add-interactivity-svelte-uis/demo/after/svelte-library/src/detail/Detail.svelte
@@ -1,5 +1,15 @@
 <script>
+  import { onMount } from "svelte";
+
+  import { httpGet } from "../common/api.js";
+
   export let id;
+
+  let book = {};
+  onMount(async () => {
+    const { data } = await httpGet("/" + id);
+    book = data;
+  });
 </script>
 
-<div>Detail page - {id}</div>
+<div>Detail page - {id}/{book.title}</div>
